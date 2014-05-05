5b2dd528807118af173a0388f65750238a5f5dc5
entmoot <w@y.c>
2014-05-05 23:42:29 +0200

    first solution commit

---
+++ plot2.R
@@ -0,0 +1,44 @@
+## Eploratory data analysis coursera course
+## Course project 1 solution - part2 
+## May 2014
+
+
+## if there is no data in expected location, download it 
+## and extract into folder data
+
+if(!file.exists("data"))
+{
+  dir.create("data")
+}
+if(!file.exists("data/electricity.zip"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest="data/electricity.zip", mode="wb")
+  library(utils)
+  
+  unzip("./data/electricity.zip", exdir="./data")
+}
+
+## read in only the data from row 66637 to row 69516 because there are
+## the rows with dates 2007-02-01 and 2007-02-02 (rows found using the binary search)
+
+data=read.table("data/household_power_consumption.txt", sep=";",na.strings="?",skip=66637,nrow=2880,
+                colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+## read in the header names (which were ignored in the row-skipping read above)
+headers=read.table("data/household_power_consumption.txt",sep=";",nrow=1, header=T)
+
+names(data)<-names(headers)
+
+## transform the dates and times from strings to the datetime format
+
+data$time=strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+data$Time=NULL
+data$Date=NULL
+
+## plotting the second plot to the PNG graphic device
+
+png(filename = "plot2.png",width = 480, height = 480, units = "px",type="cairo")
+
+plot(data$time,data$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,58 @@
+## Eploratory data analysis coursera course
+## Course project 1 solution - part4 
+## May 2014
+
+
+## if there is no data in expected location, download it 
+## and extract into folder data
+
+if(!file.exists("data"))
+{
+  dir.create("data")
+}
+if(!file.exists("data/electricity.zip"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest="data/electricity.zip", mode="wb")
+  library(utils)
+  
+  unzip("./data/electricity.zip", exdir="./data")
+}
+
+## read in only the data from row 66637 to row 69516 because there are
+## the rows with dates 2007-02-01 and 2007-02-02 (rows found using the binary search)
+
+data=read.table("data/household_power_consumption.txt", sep=";",na.strings="?",skip=66637,nrow=2880,
+                colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+## read in the header names (which were ignored in the row-skipping read above)
+headers=read.table("data/household_power_consumption.txt",sep=";",nrow=1, header=T)
+
+names(data)<-names(headers)
+
+## transform the dates and times from strings to the datetime format
+
+data$time=strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+data$Time=NULL
+data$Date=NULL
+
+## plotting the fourth plot to the PNG graphic device
+
+png(filename = "plot4.png",width = 480, height = 480, units = "px",type="cairo")
+par(mfrow=c(2,2))
+
+## first subplot
+plot(data$time,data$Global_active_power,type="l",ylab="Global Active Power",xlab="")
+
+## second subplot
+plot(data$time,data$Voltage,type="l",ylab="Voltage",xlab="datetime")
+
+## third subplot
+plot(data$time,data$Sub_metering_1,ylab="Energy sub metering",xlab="", type="l")
+lines(data$time,data$Sub_metering_2,col="red")
+lines(data$time,data$Sub_metering_3,col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lwd=1,bty="n", cex=0.9)
+
+## fourth subplot
+plot(data$time,data$Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime",lwd=0.5)
+
+dev.off()
